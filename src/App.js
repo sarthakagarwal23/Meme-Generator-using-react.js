6927df6dbfa435e44dfd4d7271f0437c36c5e0c5
sarthak agarwal <sarthak.agarwal97@gmail.com>
2019-05-11 05:48:28 +0530

    meme generato

--- src/App.js
+++ src/App.js
@@ -1,92 +1,15 @@
-import React, { Component } from 'react';
-
-import Trainer from './trainer/trainer';
-
-import './App.css';
-
-class App extends Component {
-  state={
-    trainer:{name:'SARTHAK AGARWAL' , course:'REACTJS'},
-    showTrainer : false,
-    trainers:[
-      {id:'abc111' ,name:'SARTHAK' , course:'MERN'},
-      {id:'abx112' , name:'RESHABH' , course:'ANGULAR'},
-      {id:'abc234' ,name:'MEHUL' , course:'REACTJS'}
-    ],
-    isBold: true ,
-    isColors:true
-  }
-
-  toggleTrainerHandler  = () => {
-    console.log("I AM CLICKED");
-    const isTrainerVisible = this.state.showTrainer;
-    this.setState({
-    showTrainer : !isTrainerVisible
-    });
-  }
-
-  deleteTrainerHandler = (tIndex)=> {
-    alert('HELLO YOU CLICKED'+" "+ tIndex);
-    const trainerList=this.state.trainers;
-    trainerList.splice(tIndex,1);
-    this.setState({
-     trainers:trainerList
-    });
-  }
-
-  render() {
-    const myStyle ={
-      backgroundColor:'blue' ,
-      padding: '10px' ,
-      border: '5px solid yellow',
-      color: 'white' 
-    } 
-
-    let trainers = null; 
-    if(this.state.showTrainer===true){
-      trainers =(
-     <div>
-     {
-       this.state.trainers.map((t,index) => {
-         return <Trainer name={t.name} course={t.course} 
-         deleteOnClick={this.deleteTrainerHandler.bind(this,index)}></Trainer>
-       })
-     }
-     </div>
-      )
-     // trainer = (
-      // <div>
-      // <Trainer name={this.state.trainer.name} 
-      // course={this.state.trainer.course}></Trainer>
-     //  </div>
-      //)
-      myStyle.backgroundColor='yellow';
-    }
-
-     
-     const myClasses = [ ];
-     if(this.state.isBold){
-       myClasses.push('color-cls');
-     }
-     if(this.state.trainers.length <=1){
-       myClasses.push('bold-cls');
-       myStyle.backgroundColor='red';
-     }
-
-
-    return (
-      <div className="App">
-       <h1>WELCOME TO REACT APP</h1>
-       <p className={myClasses.join(' ')}>HELLO , WELCOME TO REACT COMMUNITY</p>
-       <button onClick = {this.toggleTrainerHandler} style={myStyle}>
-       TOGGLE TRAINER SWITCH</button>
-       {trainers}
-
-          
-      </div>
-    );
-
-  }
+import React , {Component} from 'react';
+import Header from './Header';
+import MemeGenerator from './MemeGenerator';
+class App extends React.Component {
+ 
+  render(){
+    return(
+   <div>
+      <Header></Header>
+      <MemeGenerator></MemeGenerator> 
+   </div>
+  );
 }
-
+}
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+               <img src="https://mondrian.mashable.com/2018%252F09%252F14%252F1a%252FAllTheMemes_Header.6402f.png%252Foriginal.png?signature=CY1IjcuraEE6YiYBYpy_ZcHJ1vI="
+               height="200px" width="100%"></img>
+            </div>
+        );
+    }
+}
+
+export default Header;
