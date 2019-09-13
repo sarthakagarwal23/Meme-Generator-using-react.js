eebc0014456478761be934e783b599e0d0d5081a
Sarthak Agarwal <sarthak.agarwal97@gmail.com>
2019-09-14 02:12:46 +0530

    updated_changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {Component} from 'react';
+import React  from 'react';
 import Header from './Header';
 import MemeGenerator from './MemeGenerator';
 class App extends React.Component {
